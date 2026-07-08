d6dc562cd69af9d5cbf967ed829a019eb44b8fff
Khushboo-0609 <ks2656181@gmail.com>
2026-07-08 15:30:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+const path = require("path")
 const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("_dirname"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 let notes = [];
 let nextId = 1;
